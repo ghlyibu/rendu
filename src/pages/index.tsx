ee1aae11341e23fa9f7950a103edace2740e59c3
guohailin <1091291816@qq.com>
2023-04-24 14:40:52 +0800

    init

---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+import styles from '../styles/Home.module.css'
+
+const IndexPage: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Redux Toolkit</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>
+      qqq
+      </div>
+    </div>
+  )
+}
+
+export default IndexPage
